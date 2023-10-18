53e5640d17f5eef63490353cd1e228154a88795f
Gabriela Burger <139590197+gabrielaburger@users.noreply.github.com>
2023-10-18 12:34:25 -0300

    atualizei ex014 e criei ex015

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,1 +1,18 @@
-alert('Olá!')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora <= 12){
+    img.src = 'manha.png'
+    document.body.style.background = '#FEE7CE'
+    } else if (hora >= 12 && hora <=18){
+    img.src = 'tarde.png'
+    document.body.style.background = '#FEAA07'
+    } else {
+    img.src = 'noite.png'
+    document.body.style.background = 
+    '#293349'
+    }
+}
