10136ff67ffcdef1ffa2a8f145ea9be3a72a8750
Gabriela Burger <139590197+gabrielaburger@users.noreply.github.com>
2023-10-18 17:59:48 -0300

    atualizei o ex015 da aula12 e criei aula13

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -9,12 +9,41 @@ function verificar() {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
         if (fsex[0].checked) {
             genero = 'Homem'
+            if (idade >=0 && idade <10){
+                //criança
+                img.setAttribute('src', 'bebeboy.png')
+            } else if (idade <21){
+                //jovem
+                img.setAttribute('src', 'menino.png')
+            } else if (idade <50){
+                //adulto
+                img.setAttribute('src', 'adulto.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'idoso.png')
+            }
         } else if (fsex[1].checked) {
             genero = 'Mulher'
+            if (idade >=0 && idade <10){
+                //criança
+                img.setAttribute('src', 'bebegirl.png')
+            } else if (idade <21){
+                //jovem
+                img.setAttribute('src', 'menina.png')
+            } else if (idade <50){
+                //adulta
+                img.setAttribute('src', 'adulta.png')
+            } else {
+                //idosa
+                img.setAttribute('src', 'idosa.png')
+            }
         }
         res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
     }
 }
---
+++ aula13/ambiente.js
@@ -0,0 +1,31 @@
+/*
+var c = 1
+console.log(`Passo ${c}`)
+console.log(`Passo ${c}`)
+console.log(`Passo ${c}`)
+console.log(`Passo ${c}`)
+console.log(`Passo ${c}`)
+console.log(`Passo ${c}`)
+*/
+
+/*var c = 1
+while (c <= 6){
+    console.log(`Passo ${c}`)
+    c++
+}
+
+passo 1
+passo 2
+passo 3
+passo 4
+passo 5
+passo 6
+*/
+
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <=6)
+
+// As 3 formas estão corretas, porém a primeira não é viável, a segunda é a extrutura de Repetição com Teste Lógico no Início e a terceira é a extrutura de Repetição com Teste Lógico no Final.
